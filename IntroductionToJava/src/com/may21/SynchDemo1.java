cca376b0f4368951e2adf93b5b67e2b28b566807
Gaurav Ravindra Kadwadkar <gauravkadwadkar9@gmail.com>
2024-05-22 13:24:18 +0530

    Add files via upload

---
+++ IntroductionToJava/src/com/may21/SynchDemo1.java
@@ -0,0 +1,10 @@
+package com.may21;
+
+public class SynchDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ IntroductionToJava/src/com/may21/WaitNotify.java
@@ -0,0 +1,10 @@
+package com.may21;
+
+public class WaitNotify {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
